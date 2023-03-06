27109375960b06d1962d85a8dc5a4b254dbd8708
wolskikarol <115709515+wolskikarol@users.noreply.github.com>
2023-03-06 19:19:44 +0100

    PWIR_02

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <cstdio>
+#include <thread>
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int action(int id, int time) {
+	printf("Uruchamiam watek %d\n", id);
+	Sleep(time * 1000); //10 sekund
+	printf("Koncze watek %d\n", id);
+	return 0;
+}
+
+int main() {
+	//tworzenie wątku
+	std::thread t1(action, 1,5); //konstruktor klasy t1 przyjmuje minimum wskaźnik na funckje do wykonania
+	std::thread::id t1_id = t1.get_id();
+	std::thread t2(action, 2,10); //funckja ta może coś zwracać, ale może zwaracać też void
+	std::thread::id t2_id = t2.get_id();
+	std::thread t3(action, 3,2); //dalsze parametry zostaną przekazane do podanej funckji
+	std::thread::id t3_id = t3.get_id();
+	std::cout << "t1 id: " << t1_id << '\n';
+	std::cout << "t2 id: " << t2_id << '\n';
+	std::cout << "t3 id: " << t3_id << '\n';
+
+	t1.join();  //synchronizacja
+	t2.join();  //wątek główny ma tu poczekać na te 3 wątki
+	t3.join();  //inaczej program by się zakończył wcześniej bo wątki trwają minimum 10 sekund
+
+	printf("Koniec programu \r\n");
+
+	return 0;
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,42 @@
+﻿#include <cstdio>
+#include <thread>
+#include <windows.h>
+#include <iostream>
+#include <vector>
+
+
+void action(int id) {
+	printf("Uruchamiam watek %d\n", id);
+	Sleep(5 * 1000); //5 sekund
+	printf("Koncze watek %d\n", id);
+}
+
+int main() {
+	int thread_count = 0;
+	std::cout << "Podaj ilosc watkow" << std::endl;
+	std::cin >> thread_count;
+
+	std::vector<std::thread*> threads(thread_count);
+
+	//otwieranie wątków
+	for (int i = 0; i < thread_count; i++) {
+		threads[i] = new std::thread(action, i); //wykorzystuje i jako id danego wątku
+	}
+
+	//watki pracują, ale trzeba je zsynchronizować
+	for (int i = 0; i < thread_count; i++) {
+		threads[i]->join();
+	}
+
+	//alokowaliśmy pamięć więc pasuje ją zwolnić
+	for (int i = 0; i < thread_count; i++) {
+		delete threads[i];
+	}
+	delete[] threads;
+
+	printf("Koniec programu \r\n");
+
+	return 0;
+}
+
+
