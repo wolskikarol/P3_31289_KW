5e4cb6feb2f66bf846872f691a195a479b12f9f4
wolskikarol <115709515+wolskikarol@users.noreply.github.com>
2023-03-26 21:30:03 +0200

    PWIR_05

---
+++ PWIR_05/KOD7-8.cpp
@@ -0,0 +1,59 @@
+﻿#include <thread>
+#include <cstdio>
+#include <windows.h>
+#include <mutex>
+#include <chrono>
+
+std::mutex counter_mutex;
+unsigned int counter = 0;
+
+void increment() {
+    int count = 0;
+    for (;;) {
+        counter_mutex.lock();
+        counter++;
+        counter_mutex.unlock();
+        Sleep(2000);
+        count++;
+        if (count == 10) {  
+            break;
+        }
+    }
+}
+
+void parity() {
+    int count = 0;
+    for (;;) {
+        counter_mutex.lock();
+        if (counter % 2) {
+            printf("%u jest nieparzyste\r\n", counter);
+        }
+        else {
+            printf("%u jest parzyste\r\n", counter);
+        }
+        counter_mutex.unlock();
+        Sleep(2000);
+        count++;
+        if (count == 10) {  
+            break;
+        }
+    }
+}
+
+int main() {
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    std::thread inc(increment);
+    std::thread par(parity);
+
+    inc.join();
+    par.join();
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+    printf("Done\r\n");
+    printf("Czas wykonania programu: %lld ms\r\n", duration.count());
+
+    return 0;
+}
---
+++ PWIR_05/KOD9.cpp
@@ -0,0 +1,43 @@
+﻿#include <thread>
+#include <cstdio>
+#include <windows.h>
+#include <iostream>
+
+const int NUM_THREADS = 10;
+const int ARRAY_SIZE = 100;
+
+unsigned int counter = 0;
+int tablica[ARRAY_SIZE];
+
+void increment(int id) {
+    int sum = 0;
+    for (int i = id * 10; i < (id + 1) * 10; i++) {
+        sum += tablica[i];
+    }
+    counter += sum;
+    thread_local unsigned int local_counter = 0;
+    local_counter += sum;
+    Sleep(300);
+    printf("Watek %d -> %u\n", id, local_counter);
+}
+
+int main() {
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        tablica[i] = rand() % 10 + 1;
+    }
+
+    std::thread threads[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        threads[i] = std::thread(increment, i);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        threads[i].join();
+    }
+
+    printf("Suma globalna: %u\n", counter);
+
+    return 0;
+}
