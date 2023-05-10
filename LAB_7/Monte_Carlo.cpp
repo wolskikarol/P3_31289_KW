75e268694e05e1b36c39b0060918b76b16174876
wolskikarol <115709515+wolskikarol@users.noreply.github.com>
2023-05-10 16:47:24 +0200

    Add files via upload

---
+++ LAB_7/Monte_Carlo.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <thread>
+#include <vector>
+
+
+void monteCarloThread(int num_points, int& num_inside_circle) {
+    std::srand(std::time(nullptr));
+
+    for (int i = 0; i < num_points; i++) {
+        double x = ((double)std::rand() / RAND_MAX) * 2 - 1;
+        double y = ((double)std::rand() / RAND_MAX) * 2 - 1;
+        if (std::pow(x, 2) + std::pow(y, 2) <= 1) {
+            num_inside_circle++;
+        }
+    }
+}
+
+int main() {
+    const int num_points = 10000000;
+    int num_inside_circle = 0;
+    int num_threads = std::thread::hardware_concurrency();
+
+    std::vector<std::thread> threads;
+
+   
+    for (int i = 0; i < num_threads; i++) {
+        int points_per_thread = num_points / num_threads;
+        threads.emplace_back(monteCarloThread, points_per_thread, std::ref(num_inside_circle));
+    }
+
+ 
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
+
+    double pi = 4.0 * num_inside_circle / num_points;
+    std::cout << "Approximate value of Pi: " << pi << std::endl;
+
+    return 0;
+}
---
+++ LAB_7/obliczanie_silni.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+
+void factorialThread(const std::vector<int>& numbers, int startIndex, int endIndex, std::vector<long long>& results) {
+    for (int i = startIndex; i < endIndex; i++) {
+        int number = numbers[i];
+        long long factorial = 1;
+
+        for (int j = 1; j <= number; j++) {
+            factorial *= j;
+        }
+
+        results[i] = factorial;
+    }
+}
+
+int main() {
+    const int vectorSize = 10;
+    const int numThreads = std::thread::hardware_concurrency();
+
+    std::vector<int> numbers = { 5, 3, 6, 4, 7, 2, 8, 1, 9, 10 };
+    std::vector<long long> factorials(vectorSize);
+    std::vector<std::thread> threads(numThreads);
+    int chunkSize = vectorSize / numThreads;
+    int startIndex = 0;
+
+    for (int i = 0; i < numThreads; i++) {
+        int endIndex = startIndex + chunkSize;
+        if (i == numThreads - 1) {
+            endIndex = vectorSize;
+        }
+
+        threads[i] = std::thread(factorialThread, std::ref(numbers), startIndex, endIndex, std::ref(factorials));
+        startIndex = endIndex;
+    }
+
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
+
+    for (int i = 0; i < vectorSize; i++) {
+        std::cout << "Factorial of " << numbers[i] << ": " << factorials[i] << std::endl;
+    }
+
+    return 0;
+}
